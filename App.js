1be2efa7ec6b8d65634831e8227a8de9f2fb7fd0
Fortune00 <tasha0415@naver.com>
2022-02-10 03:47:47 +0900

    update : add button to start and stop

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
 import RNSoundLevel from 'react-native-sound-level';
 
 export default App => {
-  useEffect(() => {
-    RNSoundLevel.start();
-    RNSoundLevel.onNewFrame = data => {
-      console.log('Sound level info', data);
-    };
-  }, []);
+  const [sound, setSound] = useState(false);
 
-  useEffect(() => {
-    return () => {
+  function soundLevel() {
+    if (!sound) {
+      RNSoundLevel.start();
+      setSound(true);
+      RNSoundLevel.onNewFrame = data => {
+        console.log('Sound level info', data);
+      };
+    } else {
+      setSound(false);
       RNSoundLevel.stop();
-    };
-  });
+    }
+  }
+
   return (
     <View>
-      <Text>Good</Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#9dbad1',
+          borderRadius: 40,
+          marginTop: 100,
+          marginLeft: 50,
+          width: 300,
+          height: 100,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={soundLevel}>
+        <Text style={{fontSize: 20}}>SoundLevel</Text>
+      </TouchableOpacity>
     </View>
   );
 };
