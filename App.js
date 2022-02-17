9a2c2def3a3feedfe843915700cfccf98ec5dd88
Fortune00 <tasha0415@naver.com>
2022-02-18 03:39:31 +0900

    feat : play audio file

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 import RNSoundLevel from 'react-native-sound-level';
+import SoundPlayer from 'react-native-sound-player';
 
 export default App => {
   const [sound, setSound] = useState(false);
@@ -18,6 +19,14 @@ export default App => {
     }
   }
 
+  function printSound() {
+    try {
+      SoundPlayer.playSoundFile('wakeup', 'mp3');
+    } catch (e) {
+      console.log(`cannot play the sound file`, e);
+    }
+  }
+
   return (
     <View>
       <TouchableOpacity
@@ -31,7 +40,7 @@ export default App => {
           alignItems: 'center',
           justifyContent: 'center',
         }}
-        onPress={soundLevel}>
+        onPress={printSound}>
         <Text style={{fontSize: 20}}>SoundLevel</Text>
       </TouchableOpacity>
     </View>
